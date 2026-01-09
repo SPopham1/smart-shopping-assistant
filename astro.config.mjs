91f5dcfd6148990b09179d6ceb8ccbaf5e22635e
SPopham1 <pophamsamuel@gmail.com>
2026-01-09 22:16:46 +0000

    still trying to fix base site issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://spopham1.github.io",
-  base: "/smart-shopping-assistant",
+  base: "/smart-shopping-assistant/",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
